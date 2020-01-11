36d1e288e9c72532e726c87d5f93dbdbc18faad8
Chunlin Feng <46469854+ChunlinFeng@users.noreply.github.com>
2020-01-10 21:35:41 -0600

    Sweep Miners (Console Version) v0.2 ======================================== 1. initialization finished (can change position and mark flag/unknown now) 2. can click the pos block now
    ========================================
    TODO:
    1. debug Click method: figure out the correct way of exploration
    2. Add next round and Win!

--- bin/SweepMiner.py
+++ bin/SweepMiner.py
@@ -4,7 +4,7 @@ class Map_status():
     def __init__(self):
         self.status = {
             'waiting': ('o', 0),
-            "clicked": ('x', 1),
+            "clicked": (' ', 1),
             "unknown": ('?', 2),
             "flag": ('P', 3),
             "miner": ('#', 4),
@@ -13,28 +13,35 @@ class Map_status():
 
 
 class game():
-    def __init__(self, hard = 'Normal'):
+    def __init__(self, hard='Normal'):
         status = Map_status()
         self.status_sign = status.status
         self.status_mark = {}
-        for key in self.status_sign.keys(): self.status_mark [self.status_sign[key][1]] = self.status_sign[key][0]
-        self.pos = [0,0]
+        for key in self.status_sign.keys():
+            self.status_mark[self.status_sign[key][1]] = self.status_sign[key][0]
 
+        self.pos = [0, 0]
+        self.hard = hard
         self.game_running = False
-
-        if hard == 'Normal':
-            self.MAP_SIZE = 9
-            self.MINERS_SIZE = 10
-            self._hidden_map = self.__drop_miners()
-            self.map = [[self.status_sign['waiting'][1] for i in range(self.MAP_SIZE)] for j in range(self.MAP_SIZE)]
+        self.miner_initialize = False
 
     def __repr__(self):
         return self.__map_converter()
 
+    def __game_initialize(self):
+        if self.hard == 'Normal':
+            self.MAP_SIZE = 9
+            self.MINERS_SIZE = 10
+            self._hidden_map = None
+            self.map = [[self.status_sign['waiting'][1] for i in range(self.MAP_SIZE)] for j in range(self.MAP_SIZE)]
+
+        self.game_running = True
+
     def __drop_miners(self):
         # generate random int for miners position
         dic = set()
-        up_limit, down_limit = self.MAP_SIZE **2 - 1, 0
+        dic.add(self.pos[0]+9*self.pos[1])  # exclude the first clicked position
+        up_limit, down_limit = self.MAP_SIZE ** 2 - 1, 0
         count = self.MINERS_SIZE
         while count > 0:
             temp = random.randint(down_limit, up_limit)
@@ -42,13 +49,16 @@ class game():
                 dic.add(temp)
                 count -= 1
 
+        self.miner_initialize = True
+
+
         # initialize self.hidden_map
         hidden_map = [[False for i in range(self.MAP_SIZE)] for j in range(self.MAP_SIZE)]
         for num in dic:
-            x,y = num % (self.MAP_SIZE), num // (self.MAP_SIZE)
-            hidden_map[x][y] = True
+            x,y = num % self.MAP_SIZE, num // self.MAP_SIZE
+            hidden_map[x][y] = True if [x,y] != self.pos else False
 
-        return hidden_map
+        self._hidden_map = hidden_map
 
     def __map_converter(self):
         Str = ' |'
@@ -57,11 +67,11 @@ class game():
         for i in range(self.MAP_SIZE):
             Str += str(i) + '|'
             for j in range(self.MAP_SIZE):
+                mark = self.status_mark[self.map[i][j]] if isinstance(self.map[i][j], int) else self.map[i][j]
                 if [i, j] == self.pos:
-                    mark = self.status_mark[self.map[i][j]]
                     Str += '\033[1;30;43m' + mark + '\033[0m' + '|'
                 else:
-                    Str += self.status_mark[self.map[i][j]] + '|'
+                    Str += mark + '|'
             Str += "\n"
         return Str
 
@@ -76,21 +86,20 @@ class game():
                 if prev: command[-1] += ch
                 else: command.append(ch)
                 prev = True
-            elif ch == ' ':
-                prev = False
-            else: break
+            elif ch in [' ', ',', '.']:  prev = False
+            else:  break
             if len(command) == 2: return command
 
         player_input = player_input.split()
+        if len(player_input) == 0: return ['click']
         if player_input[0] in ['flag', 'unknown']: return [player_input[0]]
-        elif player_input[0] in ['click','\n']: return ['click']
         elif player_input[0] in ['Q','q']: return ['Quit']
         else: return []
 
     def __update(self, player_input):
         command = self.__parse_input(player_input)
         if len(command) == 2:
-            self.pos = [int(command[0]), int(command[1])]
+            self.pos = [int(command[0]), int(command[1])] if int(command[0]) < self.MAP_SIZE and int(command[1]) < self.MAP_SIZE else self.pos
         elif len(command) == 1:
             if command[0] == 'Quit':
                 self.game_running = False
@@ -98,18 +107,57 @@ class game():
             elif command[0] in ['flag', 'unknown']:
                 print(self.status_sign[command[0]][1])
                 self.map[self.pos[0]][self.pos[1]] = self.status_sign[command[0]][1]
+
             # TODO
-            elif command[0] in ['click', 'Enter']:
-                self.click()
+            elif command[0] in ['click']:
+                self.__click()
 
     # TODO click the pos
     def __click(self):
-        pass
+        if self.miner_initialize == False:
+            self.__drop_miners()
+            self.miner_initialize = True
+
+        if self.map[self.pos[0]][self.pos[1]] in [self.status_sign['clicked'][1]]:
+            return None
+        elif self._hidden_map[self.pos[0]][self.pos[1]]:
+            self.map[self.pos[0]][self.pos[1]] = self.status_sign['miner'][1]
+            self.show_map()
+            self.miner_initialize = False
+            self.game_running = False
+            print('Game Over')
+        else:
+            self.map[self.pos[0]][self.pos[1]] = self.status_sign['clicked'][1]
+            stack = [[self.pos[0],self.pos[1]]]
+            miner_options = {(+1,+1), (+1,-1), (+1,0), (0,+1), (0,-1), (-1, 0), (-1, -1), (-1, +1)}
+            next_step_options = {(+1,0), (0,+1), (0,-1), (-1, 0)}
+            while stack:
+                x,y = stack.pop()
+                for option in next_step_options:
+                    _x, _y = x+option[0], y+option[1]
+                    if 0 <= _x < self.MAP_SIZE and 0 <= _y < self.MAP_SIZE and self.map[_x][_y] not in [self.status_sign['clicked'][1], self.status_sign['miner'][1]]:
+                        miner_count = 0
+                        explore_next = True
+                        # TODO judge the explore series
+                        for op in miner_options:
+                            op_x, op_y = _x + op[0], _y + op[1]
+                            if 0<= op_x < self.MAP_SIZE and 0 <= op_y < self.MAP_SIZE and self._hidden_map[op_x][op_y] == True: miner_count += 1
+                            # if 0<= op_x < self.MAP_SIZE and 0 <= op_y < self.MAP_SIZE and isinstance(self.map[op_x][op_y], str): explore_next = True
+                        if miner_count == 0 and explore_next == True:
+                            stack.append([_x,_y])
+                            self.map[_x][_y] = self.status_sign['clicked'][1]
+                        elif miner_count > 0:
+                            self.map[_x][_y] = str(miner_count)
+
+
+                        # if miner_count == 0 and
+
 
 
     def game_start(self):
         # start game
-        self.game_running = True
+        self.__game_initialize()
+
         print('Input format:\n'
               'num1 num2 -> to change current position\n'
               'flag/unknown -> to mark the position\n'
